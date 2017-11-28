43b4d69846ecf48a15eb4ad19b50e09cff14e259
Amit Prakash <aprakash@internal.tavisca.com>
2017-11-28 17:39:41 +0530

    added basic implementation of exel file reader

---
+++ FileDataReader/SampleExcelReaderProj/City.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleExcelReaderProj
+{
+    public class City
+    {
+        public string  CityName { get; set; }
+
+        public string  StateCode { get; set; }
+
+        public string  CountryCode { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public string IataCityCode { get; set; }
+
+        public string FullTextColumn { get; set; }
+
+        public string TimeZoneMAppingId { get; set; }
+    }
+}
---
+++ FileDataReader/SampleExcelReaderProj/ExelFileReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SampleExcelReaderProj
+{
+    class ExelFileReader
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application xlApp = new Excel.Application();
+
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(Path.GetFullPath(@"C:\Users\aprakash\Desktop\CLTS_Cities_Data.xlsx"));
+
+            Excel.Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Sheets.get_Item(1);
+
+            Excel.Range xlRange = xlWorksheet.UsedRange; 
+
+            object[,] valueArray = (object[,])xlRange.get_Value(
+                        Excel.XlRangeValueDataType.xlRangeValueDefault);
+
+            for (int row = 1; row <= xlWorksheet.UsedRange.Rows.Count; ++row)
+            {
+                for (int col = 1; col <= xlWorksheet.UsedRange.Columns.Count; ++col)
+                {
+                    if (valueArray[row, col] != null)
+                        Console.Write(valueArray[row, col].ToString());
+                        Console.Write("     ");
+                }
+                Console.WriteLine();
+               
+            }
+
+            xlWorkbook.Close(false);
+
+            xlApp.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
